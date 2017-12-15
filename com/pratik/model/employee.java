40dbb37cc1f7f4ad50bfe28096da2cea383d7b2a
Pratik Mahadev Dimble <31777260+pratikdimble@users.noreply.github.com>
2017-12-15 23:41:36 +0530

    Create employee.java

---
+++ com/pratik/model/employee.java
@@ -0,0 +1,25 @@
+package com.pratik.entity;
+
+public class employee {
+
+	private int empId;
+	private String empName;
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	@Override
+	public String toString() {
+		return "employee [empId=" + empId + ", empName=" + empName + "]";
+	}
+	
+	
+}
